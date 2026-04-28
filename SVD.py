20d051910d3942ad2183d4e1c6f32c1945fe9c5d
Jeff <130271245+JeffHF@users.noreply.github.com>
2026-04-28 11:25:12 +0800

    Update SVD.py

--- SVD.py
+++ SVD.py
@@ -1,39 +1,36 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.datasets import load_iris
-from sklearn.decomposition import TruncatedSVD
-
 
 def SVD(X, n_dim):
     """
     :param X: (n_samples, n_features)
     :param n_dim: target dimensions
     :return:
-        data_ndim: (n_samples, n_dim)
         U: (n_samples, n_dim)
         Sigma: (n_dim, n_dim)
         VT: (n_dim, n_features)
     """
+    X = np.asarray(X, dtype=float)
     X = X - np.mean(X, axis=0, keepdims=True)
-
     U, s, VT = np.linalg.svd(X, full_matrices=False)
-
     U = U[:, :n_dim]
     s = s[:n_dim]
     VT = VT[:n_dim, :]
-
     Sigma = np.diag(s)
-    data_ndim = np.dot(U, Sigma)
 
-    return data_ndim, U, Sigma, VT
+    return U, Sigma, VT
 
 
 if __name__ == '__main__':
-    iris = load_iris()
-    X = iris.data
-    Y = iris.target
+    import matplotlib.pyplot as plt
+    from sklearn.datasets import load_digits
+    from sklearn.decomposition import TruncatedSVD
 
-    data_2d1, U, Sigma, VT = SVD(X, 2)
+    data = load_digits()
+    X = data.data
+    Y = data.target
+
+    U, Sigma, VT = SVD(X, 2)
+    data_2d1 = np.dot(U, Sigma)
     data_2d2 = TruncatedSVD(n_components=2, random_state=0).fit_transform(X)
 
     plt.figure(figsize=(8, 4))
